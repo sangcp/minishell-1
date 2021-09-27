95ece19a899e7c75dc958210b1854b1ae316d84c
sungdae-jin <makonle@naver.com>
2021-09-27 18:12:42 +0900

    Add files via upload

---
+++ minishell.h
@@ -0,0 +1,17 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <errno.h>
+# include "libft/libft.h"
+
+typedef struct s_ef{
+	size_t	row;
+	size_t	i;
+}				t_ef;
+
+#endif
